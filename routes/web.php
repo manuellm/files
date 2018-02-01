c8d6d31bf9de083197407ab9b12ab6777947ae6c
Manuel <manuellm@ymail.com>
2018-02-01 00:04:33 -0600

    subir archivos

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Auth::routes();
+Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('formulario', 'StorageController@index');
+Route::post('storage/create', 'StorageController@save');
+
+Route::get('storage/{archivo}', function ($archivo) {
+    $public_path = public_path();
+    $url = $public_path . '/storage/' . $archivo;
+    //verificamos si el archivo existe y lo retornamos
+    if (Storage::exists($archivo)) {
+        return response()->download($url);
+    }
+    //si no se encuentra lanzamos un error 404.
+    abort(404);
+});
